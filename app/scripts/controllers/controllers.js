7199adc2f83ed17193e5b2b80c7d53133aa4bfc5
Althamash <althamash.b@gmail.com>
2013-09-30 12:44:08 +0530

    minor changes

--- app/scripts/controllers/controllers.js
+++ app/scripts/controllers/controllers.js
@@ -28,10 +28,16 @@ app.controller('navCtrl', function ($scope, $location) {
 	$scope.changeView = function(view){
     $scope.$apply($location.path(view));
   };
+  $scope.$on("$routeChangeSuccess",function(){
+    $scope.Init();
+  });
+  
   $scope.path = 'home';
   $scope.Init = function(){
+    console.log("init called!!");
+    console.log($location.path());
   	$scope.path = $location.path().substr(1);
-  	$scope.path === '' ? $scope.path ='home' : $scope.path; 
+  	$scope.path === '' ? $scope.path ='home' : $scope.path;
   };
 });
 
