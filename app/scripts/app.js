b9a276a3f3beb6ca2ab450e61538ce5dd7892821
Althamash <althamash.b@gmail.com>
2013-10-04 11:48:38 +0530

    Full connect page and labs page

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('angexpApp', [])
+var app = angular.module('angexpApp', [])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
@@ -21,9 +21,15 @@ angular.module('angexpApp', [])
       })
       .when('/labs', {
         templateUrl: 'views/labs.html',
-        controller: 'homeCtrl'
+        controller: 'labsCtrl'
+      })
+      .when('/labs/:demo', {
+        templateUrl: 'views/labs.html',
+        controller: 'labsCtrl',
+        action: "labs.demos"
       })
       .when('/connect', {
+        action: null,
         templateUrl: 'views/connect.html',
         controller: 'homeCtrl'
       })
@@ -48,3 +54,145 @@ angular.module('angexpApp', [])
         redirectTo: '/'
       });
   });
+
+/*
+--------------------------------
+  Controllers
+--------------------------------
+*/
+
+app.controller('homeCtrl', function ($scope) {
+  
+});
+
+app.controller('aboutCtrl', function ($scope, $http) {
+  $scope.getTimelineData = function(){
+    $http.get('scripts/timeline.json').success(function(data){
+      $scope.timelineData = data.data;
+    });
+  }
+});
+
+app.controller('workCtrl', function ($scope) {
+  $scope.langGraphLoaded = false;
+  $scope.otherGraphLoaded = false;
+  $scope.langGraph = function(){
+    $scope.langGraphLoaded = true;
+  }
+});
+
+app.controller('navCtrl', function ($scope, $location) {
+  $scope.changeView = function(view){
+    $scope.$apply($location.path(view));
+  };
+  $scope.$on("$routeChangeSuccess",function(){
+    $scope.Init();
+  });
+  
+  $scope.path = 'home';
+  $scope.Init = function(){
+    $scope.path = $location.path().substr(1);
+    $scope.path === '' ? $scope.path ='home' : $scope.path;
+  };
+});
+
+app.controller('asideCtrl', function ($scope) {
+  
+});
+
+app.controller('labsCtrl', function ($scope, $http, $route, $routeParams) {
+  $scope.showDemoList = true;
+  $scope.showDemo = false;
+  $scope.ifSrc = '';
+
+  var isEmpty = function(obj){
+    var hasProp = Object.prototype.hasOwnProperty;
+    // null and undefined are empty
+    if (obj == null) return true;
+    // Assume if it has a length property with a non-zero value
+    // that that property is correct.
+    if (obj.length && obj.length > 0){
+      return false;
+    }    
+    if (obj.length === 0){
+      return true;
+    }  
+    for (var key in obj) {
+      if (hasOwnProperty.call(obj, key)){
+        return false;
+      }    
+    }
+    // Doesn't handle toString and toValue enumeration bugs in IE < 9
+    return true;
+  }
+
+  $scope.$on("$routeChangeSuccess",function(){
+    if(!isEmpty($routeParams)){
+      $scope.showDemoList = false;
+      $scope.showDemo = true;
+      // console.log($route.current.action);
+      // console.log($route.current.action.split( "." ));
+      // console.log($routeParams.demo || "");
+      $scope.ifSrc = 'views/demos/' + ($routeParams.demo || "") + '.html';
+    }
+  });
+});
+
+/*
+--------------------------------
+  Directives
+--------------------------------
+*/
+
+app.directive('navAction', function(){
+  return {
+    restrict: 'A',
+    link: function(scope, element, attrs){
+      element.bind('click', function(){
+        scope.selectNav(attrs.navAction);
+        scope.changeView(attrs.navAction);
+      });
+      scope.selectNav = function(view){
+        var navLength = element.parent().find('li').length;
+        var navArray = [];
+        for(var i = 0; i < navLength; i++){
+          navArray.push(element.parent().children().eq(i));
+        }
+        for(var i = 0; i < navLength; i++){
+          if(navArray[i].hasClass('active')){
+            navArray[i].removeClass('active');
+          } else if(navArray[i].hasClass(view)) {
+            navArray[i].addClass('active');
+          }
+        }
+      };
+    }
+  };
+});
+
+app.directive('initGraph', function($window){
+  return {
+    restrict: 'A',
+    link: function(scope, element, attrs){
+      angular.element($window).bind('scroll', function(e){
+        if(this.pageYOffset > 500){
+          scope.otherGraphLoaded = true;
+          scope.$apply();
+        }
+      });
+    }
+  };
+});
+
+app.directive('isLoaded', function(){
+  return {
+    restrict: 'A',
+    link: function(scope, element, attrs){
+      element.bind('load', function(){
+        this.style.height = this.contentWindow.document.body.scrollHeight + 'px'
+      });
+    }
+  }
+});
+
+
